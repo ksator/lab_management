388f8cafb11a3761bb00756e4c92b072067de5f8
root <root@awx1.amerpoc.jnpr.net>
2017-12-27 08:24:58 -0500

    m

--- python/credentials.py
+++ python/credentials.py
@@ -3,8 +3,9 @@ def ansible_credentials_to_python_credentials():
 	from pprint import pprint as pp
 
 	credentials_file = open('group_vars/JUNOS/credentials.yml', "r")
-	credentials_file_content = credentials_file.read()
-	credentials_file_content_in_yaml = yaml.load(credentials_file_content)
+        credentials_file_content = credentials_file.read()
+	credentials_file.close()
+        credentials_file_content_in_yaml = yaml.load(credentials_file_content)
 
 	'''
 	pp(credentials_file_content_in_yaml)
